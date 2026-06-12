ff1ac3b2afa022783daa067bcc64ec7daeb69010
同创合一 <git@zerdoor.com>
2026-06-12 10:41:47 +0800

    chore: release v0.1.3

--- homebrew/brokr.rb
+++ homebrew/brokr.rb
@@ -1,19 +1,19 @@
 class Brokr < Formula
   desc "AI-safe credential broker CLI"
   homepage "https://github.com/Furowu/brokr"
-  version "0.1.2"
+  version "0.1.3"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/Furowu/brokr/releases/download/v0.1.2/brokr-x86_64-apple-darwin.tar.gz"
+    url "https://github.com/Furowu/brokr/releases/download/v0.1.3/brokr-x86_64-apple-darwin.tar.gz"
     sha256 "PLACEHOLDER"
   elsif OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/Furowu/brokr/releases/download/v0.1.2/brokr-aarch64-apple-darwin.tar.gz"
+    url "https://github.com/Furowu/brokr/releases/download/v0.1.3/brokr-aarch64-apple-darwin.tar.gz"
     sha256 "PLACEHOLDER"
   elsif OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/Furowu/brokr/releases/download/v0.1.2/brokr-x86_64-unknown-linux-gnu.tar.gz"
+    url "https://github.com/Furowu/brokr/releases/download/v0.1.3/brokr-x86_64-unknown-linux-gnu.tar.gz"
     sha256 "PLACEHOLDER"
   elsif OS.linux? && Hardware::CPU.arm?
-    url "https://github.com/Furowu/brokr/releases/download/v0.1.2/brokr-aarch64-unknown-linux-gnu.tar.gz"
+    url "https://github.com/Furowu/brokr/releases/download/v0.1.3/brokr-aarch64-unknown-linux-gnu.tar.gz"
     sha256 "PLACEHOLDER"
   end
 
