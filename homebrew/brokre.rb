960a37bf351f07ee2b09501e8fad685aae4f6c3c
Furowu <furowu@qq.com>
2026-09-24 11:33:18 +0800

    chore: homebrew sha256 v0.2.35

--- homebrew/brokre.rb
+++ homebrew/brokre.rb
@@ -5,16 +5,16 @@ class Brokre < Formula
 
   if OS.mac? && Hardware::CPU.intel?
     url "https://github.com/Furowu/brokre/releases/download/v0.2.35/brokre-x86_64-apple-darwin.tar.gz"
-    sha256 "47e9b3da4aa827ce8f3a41ba75e1575e08b466148e78caa8cb225fba1f21d673"
+    sha256 "86a0682ccc6404bfb91cd768c6b17cf31c9d97a57d94070bd20b4b3048a95fd8"
   elsif OS.mac? && Hardware::CPU.arm?
     url "https://github.com/Furowu/brokre/releases/download/v0.2.35/brokre-aarch64-apple-darwin.tar.gz"
-    sha256 "643d4b176b0bb2c1711b7a64534fb2677bfe72eb23b8bb99a37ef6baa0f00e1f"
+    sha256 "4b3a92d5d6199dcd2d939ecb166d560dc34a641b0d7728a06fb5633dbbfd9f00"
   elsif OS.linux? && Hardware::CPU.intel?
     url "https://github.com/Furowu/brokre/releases/download/v0.2.35/brokre-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "d315d2c6bea4d469a0d126701699cde7ce4cd001282f50e5217be1f0ef3dffbb"
+    sha256 "df4848beb1e3ad7f1273d9131880f8b2f35f81c80ace5a52c1dbc04c8bb31596"
   elsif OS.linux? && Hardware::CPU.arm?
     url "https://github.com/Furowu/brokre/releases/download/v0.2.35/brokre-aarch64-unknown-linux-gnu.tar.gz"
-    sha256 "ff25ed1b03ec0c36c083595035aeaa55f81d3ee99506f0ce880172f3774f7a82"
+    sha256 "5080e52dcafb058739735ccaaae44b7d9e9146fc75c4721754ab154843c13eb1"
   end
 
   def install
